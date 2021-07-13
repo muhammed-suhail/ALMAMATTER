1d323642856a1d68b8e1af803b27bd3a85154925
Muhammed suhail <65767716+muhammed-suhail@users.noreply.github.com>
2021-07-13 12:07:47 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,18 @@
+package almamatter;
+import java.util.Scanner;
+public class PrintANumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Creates a reader instance which takes
+		// input from standard input - keyboard
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		// nextInt() reads the next integer from the keyboard
+		int number = reader.nextInt();
+		// println() prints the following line to the output screen
+		System.out.println("You entered: " + number);
+		}
+
+}
---
+++ basic.java
@@ -0,0 +1,18 @@
+package almamater;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=100;
+		int j = 200;
+		
+		int sum = i+j;
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
