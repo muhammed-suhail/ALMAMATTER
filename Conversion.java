99c3e609cba0e8209b9d748a1dd40c333ecfe3aa
Muhammed suhail <65767716+muhammed-suhail@users.noreply.github.com>
2021-07-15 11:15:19 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,16 @@
+package almamater;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte a=10;
+		byte b=2;
+		int c=b*a;
+		System.out.println(c);
+		
+
+	}
+
+}
---
+++ Ifstatement.java
@@ -0,0 +1,13 @@
+package almamater;
+
+public class Ifstatement {
+	public static void main(String[] args) {
+		int i=200;
+		if(i==100) {
+		System.out.println("i is Hundred");
+		}
+		else if(i==200) {
+		System.out.println("i is two Hundred");
+		}
+}
+	}
